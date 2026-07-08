50cf3f2ddc029f03bcb8c15d33d59175eb65cd2c
Eder Mena <edermena@Eders-MacBook-Pro.local>
2026-07-08 07:39:34 -0500

    add: CI/CD to deploy VPC

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -4,7 +4,7 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_vpc" "this " {
+resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
