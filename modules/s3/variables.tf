2ae31d591b2d74c39eedd78146325bafe29b5987
Eder Mena <edermena@Eders-MacBook-Pro.local>
2026-07-18 23:23:41 -0500

    add: new module s3

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -12,7 +12,6 @@ variable "tags" {
 variable "force_destroy" {
     description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
     type        = bool
-    default     = false
 }
 
 variable "acl" {
